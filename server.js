9e3f38854300f49f235289efab357a3dbe58dd50
Preston Badeer <prrstn@gmail.com>
2015-04-28 19:08:50 -0500

    folder structure

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var port = 3000;
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
-}).get('/script.js', function(req, res){
-  res.sendfile('script.js');
-}).get('/styles.css', function(req, res){
-  res.sendfile('styles.css');
-}).get('/reset.css', function(req, res){
-  res.sendfile('reset.css');
+}).get('/js/script.js', function(req, res){
+  res.sendfile('js/script.js');
+}).get('/css/styles.css', function(req, res){
+  res.sendfile('css/styles.css');
+}).get('/css/reset.css', function(req, res){
+  res.sendfile('css/reset.css');
 });
 
 io.on('connection', function(socket){
